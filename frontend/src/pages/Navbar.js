3141464eda47b302ae17ea1d26eb54402d52532c
Ngozi Izuchi G <gi4real.gi@gmail.com>
2020-01-24 08:31:36 +0100

    added bootstrap, popper, jquery

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css"
-import "jquery/dist/jquery.slim"
-import "popper.js"
-import "bootstrap/dist/js/bootstrap.js"
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "jquery/dist/jquery.slim";
+import "popper.js";
+import "bootstrap/dist/js/bootstrap.js";
 
 
 export default class Navbar extends React.Component {
@@ -10,28 +11,30 @@ export default class Navbar extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">Navbar</a>
 
-        <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <div className="collapse navbar-collapse " id="collapsibleNavbar">
+            <ul className="navbar-nav ml-auto">
+              <Link to="/" className="nav-item nav-link">
+                View
+            </Link>
+              <Link to="/add" className="nav-item nav-link">
+                Add
+            </Link>
+              <Link to="/update" className="nav-item nav-link">
+                Update
+            </Link>
+              <Link to="/delete" className="nav-item nav-link">
+                Delete
+            </Link>
 
-        <div className="collapse navbar-collapse " id="collapsibleNavbar">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">View</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Add</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Update</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Delete</a>
-            </li>
-          </ul>
+            </ul>
+          </div>
         </div>
       </nav>
     );
